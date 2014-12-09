cd4248d1dab1fdb98dc064265738c04000005fcf
Piotr Protas <piotrek@codequest.com>
2014-12-09 13:19:35 +0100

    Small fix

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,8 +5,8 @@ module Command
   class CLI < Escort::ActionCommand::Base
 
     def execute
-      # setting_file = YAML.load_file(global_options[:file])
-      # Migrator.new(setting_file, global_options[:exporter]).run(command_name, command_options)
+      setting_file = YAML.load_file(global_options[:file])
+      Migrator.new(setting_file, global_options[:exporter]).run(command_name, command_options)
     end
 
   end
